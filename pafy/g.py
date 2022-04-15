2300bc340dcf6f08a350d522b075c1ade0248bb9
katokbots <101113553+katokbots@users.noreply.github.com>
2022-04-16 03:02:51 +0800

    Update g.py

--- pafy/g.py
+++ pafy/g.py
@@ -21,7 +21,8 @@ urls = {
                 'eurl=https://youtube.googleapis.com/v/%s&sts=%s'),
     'embed': "https://youtube.com/embed/%s"
 }
-api_key = "AIzaSyCIM4EzNqi1in22f4Z3Ru3iYvLaY8tc3bo"
+#api_key = "AIzaSyCIM4EzNqi1in22f4Z3Ru3iYvLaY8tc3bo"
+api_key = "AIzaSyCHbL-BENsK3jKaCVKYzCU5EiVpzH6kvIA"
 user_agent = "pafy " + __version__
 lifespan = 60 * 60 * 5  # 5 hours
 opener = build_opener()
